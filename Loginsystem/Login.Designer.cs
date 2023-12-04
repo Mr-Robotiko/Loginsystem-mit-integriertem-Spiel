00ecd0b603d58cddf09baae3811c23744fcbd454
Daniel Sauer <daniel.s93@gso.schule.koeln>
2023-12-04 21:09:43 +0100

    Es wurden folgende Steuerelemenete dem Login-Fenster hinzugefügt:
    - TextBox für die Eingabe des Benutzernamens
    - TextBox für die Eingabe des Passworts
    - Button für den Anmeldeabbruch
    - Button für den Anmeldefortgang
    
    Somit ist die Nutzerbedienbarkeit im Login-Fenster
    gewährleistet.

--- Loginsystem/Login.Designer.cs
+++ Loginsystem/Login.Designer.cs
@@ -31,7 +31,16 @@
             login_panel1 = new Panel();
             login_panel2 = new Panel();
             login_panel3 = new Panel();
+            anmelden_button = new Button();
+            abbrechen_button = new Button();
+            login_panel5 = new Panel();
+            login_passwort_textBox = new TextBox();
+            login_panel4 = new Panel();
+            login_benutzername_textBox = new TextBox();
             login_panel2.SuspendLayout();
+            login_panel3.SuspendLayout();
+            login_panel5.SuspendLayout();
+            login_panel4.SuspendLayout();
             SuspendLayout();
             // 
             // login_panel1
@@ -53,10 +62,80 @@
             // login_panel3
             // 
             login_panel3.BackColor = Color.White;
+            login_panel3.Controls.Add(anmelden_button);
+            login_panel3.Controls.Add(abbrechen_button);
+            login_panel3.Controls.Add(login_panel5);
+            login_panel3.Controls.Add(login_panel4);
             login_panel3.Location = new Point(5, 5);
             login_panel3.Name = "login_panel3";
             login_panel3.Size = new Size(480, 380);
             login_panel3.TabIndex = 0;
+            // 
+            // anmelden_button
+            // 
+            anmelden_button.BackColor = Color.FromArgb(31, 82, 75);
+            anmelden_button.FlatStyle = FlatStyle.Flat;
+            anmelden_button.Font = new Font("Verdana", 16.2F, FontStyle.Regular, GraphicsUnit.Point);
+            anmelden_button.ForeColor = Color.White;
+            anmelden_button.Location = new Point(270, 322);
+            anmelden_button.Name = "anmelden_button";
+            anmelden_button.Size = new Size(200, 45);
+            anmelden_button.TabIndex = 3;
+            anmelden_button.Text = "Anmelden";
+            anmelden_button.UseVisualStyleBackColor = false;
+            // 
+            // abbrechen_button
+            // 
+            abbrechen_button.BackColor = Color.FromArgb(31, 82, 75);
+            abbrechen_button.FlatStyle = FlatStyle.Flat;
+            abbrechen_button.Font = new Font("Verdana", 16.2F, FontStyle.Regular, GraphicsUnit.Point);
+            abbrechen_button.ForeColor = Color.White;
+            abbrechen_button.Location = new Point(10, 322);
+            abbrechen_button.Name = "abbrechen_button";
+            abbrechen_button.Size = new Size(200, 45);
+            abbrechen_button.TabIndex = 2;
+            abbrechen_button.Text = "Abbrechen";
+            abbrechen_button.UseVisualStyleBackColor = false;
+            // 
+            // login_panel5
+            // 
+            login_panel5.BackColor = Color.FromArgb(31, 82, 75);
+            login_panel5.Controls.Add(login_passwort_textBox);
+            login_panel5.Location = new Point(10, 251);
+            login_panel5.Name = "login_panel5";
+            login_panel5.Size = new Size(460, 48);
+            login_panel5.TabIndex = 3;
+            // 
+            // login_passwort_textBox
+            // 
+            login_passwort_textBox.BackColor = Color.White;
+            login_passwort_textBox.BorderStyle = BorderStyle.FixedSingle;
+            login_passwort_textBox.Font = new Font("Verdana", 16.2F, FontStyle.Regular, GraphicsUnit.Point);
+            login_passwort_textBox.Location = new Point(4, 4);
+            login_passwort_textBox.Name = "login_passwort_textBox";
+            login_passwort_textBox.PlaceholderText = "Passwort";
+            login_passwort_textBox.Size = new Size(452, 40);
+            login_passwort_textBox.TabIndex = 1;
+            // 
+            // login_panel4
+            // 
+            login_panel4.BackColor = Color.FromArgb(31, 82, 75);
+            login_panel4.Controls.Add(login_benutzername_textBox);
+            login_panel4.Location = new Point(10, 173);
+            login_panel4.Name = "login_panel4";
+            login_panel4.Size = new Size(460, 48);
+            login_panel4.TabIndex = 2;
+            // 
+            // login_benutzername_textBox
+            // 
+            login_benutzername_textBox.BackColor = Color.White;
+            login_benutzername_textBox.BorderStyle = BorderStyle.FixedSingle;
+            login_benutzername_textBox.Font = new Font("Verdana", 16.2F, FontStyle.Regular, GraphicsUnit.Point);
+            login_benutzername_textBox.Location = new Point(4, 4);
+            login_benutzername_textBox.Name = "login_benutzername_textBox";
+            login_benutzername_textBox.PlaceholderText = "Benutzername";
+            login_benutzername_textBox.Size = new Size(452, 40);
+            login_benutzername_textBox.TabIndex = 0;
             // 
             // Login
             // 
@@ -75,6 +154,11 @@
             ShowIcon = false;
             StartPosition = FormStartPosition.CenterScreen;
             login_panel2.ResumeLayout(false);
+            login_panel3.ResumeLayout(false);
+            login_panel5.ResumeLayout(false);
+            login_panel5.PerformLayout();
+            login_panel4.ResumeLayout(false);
+            login_panel4.PerformLayout();
             ResumeLayout(false);
         }
 
@@ -83,5 +167,12 @@
         private Panel login_panel1;
         private Panel login_panel2;
         private Panel login_panel3;
+        private TextBox login_textBox1;
+        private TextBox login_passwort_textBox;
+        private TextBox login_benutzername_textBox;
+        private Panel login_panel4;
+        private Panel login_panel5;
+        private Button anmelden_button;
+        private Button abbrechen_button;
     }
 }
--- Loginsystem/Login.cs
+++ Loginsystem/Login.cs
@@ -20,6 +20,12 @@ namespace Loginsystem
             EckenAbrundung.RundeEcken(login_panel1, 20);
             EckenAbrundung.RundeEcken(login_panel2, 20);
             EckenAbrundung.RundeEcken(login_panel3, 20);
+            EckenAbrundung.RundeEcken(login_panel4, 10);
+            EckenAbrundung.RundeEcken(login_panel5, 10);
+            EckenAbrundung.RundeEcken(login_benutzername_textBox, 10);
+            EckenAbrundung.RundeEcken(login_passwort_textBox, 10);
+            EckenAbrundung.RundeEcken(abbrechen_button, 15);
+            EckenAbrundung.RundeEcken(anmelden_button, 15);
         }
     }
 }
