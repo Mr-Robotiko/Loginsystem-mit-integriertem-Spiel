cdf27a3544ded8e136a1177d64ecb198560d31c2
Daniel Sauer <daniel.s93@gso.schule.koeln>
2023-12-08 19:33:49 +0100

    Anlegung des Menue-Forms:
    Das Menue Form wurde erstellt. Über das Menue-Form
    wird der Nutzer die Möglichkeit haben zum Login-
    als auch zum Registrations-Fenster zu gelangen.
    Das Design ist entsprechend dem vom Login- und
    Registrieren-Form angepasst.

---
+++ Loginsystem/Menue.Designer.cs
@@ -0,0 +1,304 @@
+﻿namespace Loginsystem
+{
+    partial class Menue
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Menue));
+            menue_background_pictureBox = new PictureBox();
+            menue_schneeflocken_pictureBox1 = new PictureBox();
+            menue_panel1 = new Panel();
+            menue_panel2 = new Panel();
+            menue_schneeflocken_pictureBox2 = new PictureBox();
+            menue_beenden_button = new Button();
+            menue_panel3 = new Panel();
+            menue_panel4 = new Panel();
+            menue_panel5 = new Panel();
+            menue_pictureBox1 = new PictureBox();
+            menue_anmelden_button = new Button();
+            menue_registrieren_button = new Button();
+            menue_panel6 = new Panel();
+            menue_panel7 = new Panel();
+            menue_pictureBox3 = new PictureBox();
+            menue_pictureBox2 = new PictureBox();
+            menue_label = new Label();
+            menue_panel8 = new Panel();
+            ((System.ComponentModel.ISupportInitialize)menue_background_pictureBox).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)menue_schneeflocken_pictureBox1).BeginInit();
+            menue_panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)menue_schneeflocken_pictureBox2).BeginInit();
+            menue_panel3.SuspendLayout();
+            menue_panel4.SuspendLayout();
+            menue_panel5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)menue_pictureBox1).BeginInit();
+            menue_panel6.SuspendLayout();
+            menue_panel7.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)menue_pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)menue_pictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // menue_background_pictureBox
+            // 
+            menue_background_pictureBox.Image = (Image)resources.GetObject("menue_background_pictureBox.Image");
+            menue_background_pictureBox.Location = new Point(0, 0);
+            menue_background_pictureBox.Name = "menue_background_pictureBox";
+            menue_background_pictureBox.Size = new Size(1600, 900);
+            menue_background_pictureBox.TabIndex = 0;
+            menue_background_pictureBox.TabStop = false;
+            // 
+            // menue_schneeflocken_pictureBox1
+            // 
+            menue_schneeflocken_pictureBox1.Image = (Image)resources.GetObject("menue_schneeflocken_pictureBox1.Image");
+            menue_schneeflocken_pictureBox1.Location = new Point(179, 0);
+            menue_schneeflocken_pictureBox1.Name = "menue_schneeflocken_pictureBox1";
+            menue_schneeflocken_pictureBox1.Size = new Size(71, 900);
+            menue_schneeflocken_pictureBox1.TabIndex = 1;
+            menue_schneeflocken_pictureBox1.TabStop = false;
+            // 
+            // menue_panel1
+            // 
+            menue_panel1.BackColor = Color.White;
+            menue_panel1.Location = new Point(174, 0);
+            menue_panel1.Name = "menue_panel1";
+            menue_panel1.Size = new Size(81, 900);
+            menue_panel1.TabIndex = 2;
+            // 
+            // menue_panel2
+            // 
+            menue_panel2.BackColor = Color.White;
+            menue_panel2.Controls.Add(menue_schneeflocken_pictureBox2);
+            menue_panel2.Location = new Point(1345, 0);
+            menue_panel2.Name = "menue_panel2";
+            menue_panel2.Size = new Size(81, 900);
+            menue_panel2.TabIndex = 3;
+            // 
+            // menue_schneeflocken_pictureBox2
+            // 
+            menue_schneeflocken_pictureBox2.BackColor = Color.FromArgb(31, 82, 75);
+            menue_schneeflocken_pictureBox2.Image = (Image)resources.GetObject("menue_schneeflocken_pictureBox2.Image");
+            menue_schneeflocken_pictureBox2.Location = new Point(5, 0);
+            menue_schneeflocken_pictureBox2.Name = "menue_schneeflocken_pictureBox2";
+            menue_schneeflocken_pictureBox2.Size = new Size(71, 900);
+            menue_schneeflocken_pictureBox2.TabIndex = 2;
+            menue_schneeflocken_pictureBox2.TabStop = false;
+            // 
+            // menue_beenden_button
+            // 
+            menue_beenden_button.BackColor = Color.White;
+            menue_beenden_button.FlatStyle = FlatStyle.Flat;
+            menue_beenden_button.Image = (Image)resources.GetObject("menue_beenden_button.Image");
+            menue_beenden_button.Location = new Point(1550, 0);
+            menue_beenden_button.Name = "menue_beenden_button";
+            menue_beenden_button.Size = new Size(50, 50);
+            menue_beenden_button.TabIndex = 4;
+            menue_beenden_button.UseVisualStyleBackColor = false;
+            menue_beenden_button.Click += menue_beenden_button_Click;
+            // 
+            // menue_panel3
+            // 
+            menue_panel3.BackColor = Color.White;
+            menue_panel3.Controls.Add(menue_panel4);
+            menue_panel3.Location = new Point(550, 300);
+            menue_panel3.Name = "menue_panel3";
+            menue_panel3.Size = new Size(500, 400);
+            menue_panel3.TabIndex = 5;
+            // 
+            // menue_panel4
+            // 
+            menue_panel4.BackColor = Color.FromArgb(31, 82, 75);
+            menue_panel4.Controls.Add(menue_panel5);
+            menue_panel4.Location = new Point(5, 5);
+            menue_panel4.Name = "menue_panel4";
+            menue_panel4.Size = new Size(490, 390);
+            menue_panel4.TabIndex = 2;
+            // 
+            // menue_panel5
+            // 
+            menue_panel5.BackColor = Color.White;
+            menue_panel5.Controls.Add(menue_pictureBox1);
+            menue_panel5.Controls.Add(menue_anmelden_button);
+            menue_panel5.Controls.Add(menue_registrieren_button);
+            menue_panel5.Location = new Point(5, 5);
+            menue_panel5.Name = "menue_panel5";
+            menue_panel5.Size = new Size(480, 380);
+            menue_panel5.TabIndex = 0;
+            // 
+            // menue_pictureBox1
+            // 
+            menue_pictureBox1.Image = (Image)resources.GetObject("menue_pictureBox1.Image");
+            menue_pictureBox1.Location = new Point(165, 10);
+            menue_pictureBox1.Name = "menue_pictureBox1";
+            menue_pictureBox1.Size = new Size(150, 150);
+            menue_pictureBox1.TabIndex = 4;
+            menue_pictureBox1.TabStop = false;
+            // 
+            // menue_anmelden_button
+            // 
+            menue_anmelden_button.BackColor = Color.FromArgb(31, 82, 75);
+            menue_anmelden_button.FlatStyle = FlatStyle.Flat;
+            menue_anmelden_button.Font = new Font("Tahoma", 16.2F);
+            menue_anmelden_button.ForeColor = Color.White;
+            menue_anmelden_button.Location = new Point(10, 260);
+            menue_anmelden_button.Name = "menue_anmelden_button";
+            menue_anmelden_button.Size = new Size(460, 45);
+            menue_anmelden_button.TabIndex = 3;
+            menue_anmelden_button.Text = "Anmelden";
+            menue_anmelden_button.UseVisualStyleBackColor = false;
+            // 
+            // menue_registrieren_button
+            // 
+            menue_registrieren_button.BackColor = Color.FromArgb(31, 82, 75);
+            menue_registrieren_button.FlatStyle = FlatStyle.Flat;
+            menue_registrieren_button.Font = new Font("Tahoma", 16.2F);
+            menue_registrieren_button.ForeColor = Color.White;
+            menue_registrieren_button.Location = new Point(10, 320);
+            menue_registrieren_button.Name = "menue_registrieren_button";
+            menue_registrieren_button.Size = new Size(460, 45);
+            menue_registrieren_button.TabIndex = 2;
+            menue_registrieren_button.Text = "Registrieren";
+            menue_registrieren_button.UseVisualStyleBackColor = false;
+            // 
+            // menue_panel6
+            // 
+            menue_panel6.BackColor = Color.White;
+            menue_panel6.Controls.Add(menue_panel7);
+            menue_panel6.Location = new Point(550, 225);
+            menue_panel6.Name = "menue_panel6";
+            menue_panel6.Size = new Size(500, 80);
+            menue_panel6.TabIndex = 6;
+            // 
+            // menue_panel7
+            // 
+            menue_panel7.BackColor = Color.FromArgb(31, 82, 75);
+            menue_panel7.Controls.Add(menue_pictureBox3);
+            menue_panel7.Controls.Add(menue_pictureBox2);
+            menue_panel7.Controls.Add(menue_label);
+            menue_panel7.Controls.Add(menue_panel8);
+            menue_panel7.Location = new Point(5, 5);
+            menue_panel7.Name = "menue_panel7";
+            menue_panel7.Size = new Size(490, 70);
+            menue_panel7.TabIndex = 0;
+            // 
+            // menue_pictureBox3
+            // 
+            menue_pictureBox3.BackColor = Color.White;
+            menue_pictureBox3.Image = (Image)resources.GetObject("menue_pictureBox3.Image");
+            menue_pictureBox3.Location = new Point(425, 5);
+            menue_pictureBox3.Name = "menue_pictureBox3";
+            menue_pictureBox3.Size = new Size(60, 60);
+            menue_pictureBox3.TabIndex = 11;
+            menue_pictureBox3.TabStop = false;
+            // 
+            // menue_pictureBox2
+            // 
+            menue_pictureBox2.BackColor = Color.White;
+            menue_pictureBox2.Image = (Image)resources.GetObject("menue_pictureBox2.Image");
+            menue_pictureBox2.Location = new Point(5, 5);
+            menue_pictureBox2.Name = "menue_pictureBox2";
+            menue_pictureBox2.Size = new Size(60, 60);
+            menue_pictureBox2.TabIndex = 1;
+            menue_pictureBox2.TabStop = false;
+            // 
+            // menue_label
+            // 
+            menue_label.BackColor = Color.White;
+            menue_label.Font = new Font("Tahoma", 25.8000011F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            menue_label.ForeColor = Color.FromArgb(31, 82, 75);
+            menue_label.Location = new Point(65, 7);
+            menue_label.Name = "menue_label";
+            menue_label.Size = new Size(360, 55);
+            menue_label.TabIndex = 0;
+            menue_label.Text = "Menü";
+            menue_label.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // menue_panel8
+            // 
+            menue_panel8.BackColor = Color.White;
+            menue_panel8.Location = new Point(5, 5);
+            menue_panel8.Name = "menue_panel8";
+            menue_panel8.Size = new Size(480, 60);
+            menue_panel8.TabIndex = 5;
+            // 
+            // Menue
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(31, 82, 75);
+            ClientSize = new Size(1600, 900);
+            ControlBox = false;
+            Controls.Add(menue_panel6);
+            Controls.Add(menue_panel3);
+            Controls.Add(menue_beenden_button);
+            Controls.Add(menue_panel2);
+            Controls.Add(menue_schneeflocken_pictureBox1);
+            Controls.Add(menue_panel1);
+            Controls.Add(menue_background_pictureBox);
+            DoubleBuffered = true;
+            FormBorderStyle = FormBorderStyle.None;
+            MaximumSize = new Size(1600, 900);
+            MinimumSize = new Size(1600, 900);
+            Name = "Menue";
+            ShowIcon = false;
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Menue";
+            ((System.ComponentModel.ISupportInitialize)menue_background_pictureBox).EndInit();
+            ((System.ComponentModel.ISupportInitialize)menue_schneeflocken_pictureBox1).EndInit();
+            menue_panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)menue_schneeflocken_pictureBox2).EndInit();
+            menue_panel3.ResumeLayout(false);
+            menue_panel4.ResumeLayout(false);
+            menue_panel5.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)menue_pictureBox1).EndInit();
+            menue_panel6.ResumeLayout(false);
+            menue_panel7.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)menue_pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)menue_pictureBox2).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox menue_background_pictureBox;
+        private PictureBox menue_schneeflocken_pictureBox1;
+        private Panel menue_panel1;
+        private Panel menue_panel2;
+        private PictureBox menue_schneeflocken_pictureBox2;
+        private Button menue_beenden_button;
+        private Panel menue_panel3;
+        private Panel menue_panel4;
+        private Panel menue_panel5;
+        private PictureBox menue_pictureBox1;
+        private Button menue_anmelden_button;
+        private Button menue_registrieren_button;
+        private Panel menue_panel6;
+        private Panel menue_panel7;
+        private PictureBox menue_pictureBox3;
+        private PictureBox menue_pictureBox2;
+        private Label menue_label;
+        private Panel menue_panel8;
+    }
+}
