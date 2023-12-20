28a45c6545377147331c4cc08a1b14fb9034c80a
Raphael Tack <raphael.t7@gso.schule.koeln>
2023-12-20 13:49:09 +0100

    Es wurde ein Datenbankdokument hinzugefügt dass: - Den Skrip der Tabelle "Nutzer" beinhaltet: 	- User_ID - PK, INT 	- Name - VARCHAR(50) 	- Prename - VARCHAR(50) 	- Birthday - DATE 	- Rights_Status - CHAR 	- PASSWORD - VARCHAR(50)

---
+++ Datenbank/dbo.Nutzer.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].Nutzer
+(
+	[User_ID] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Name] NVARCHAR(50) NOT NULL, 
+    [Prename] NVARCHAR(50) NOT NULL, 
+    [Birthday] DATE NOT NULL, 
+    [Rights_Status] NCHAR(10) NOT NULL, 
+    [Password] NVARCHAR(50) NOT NULL
+)
