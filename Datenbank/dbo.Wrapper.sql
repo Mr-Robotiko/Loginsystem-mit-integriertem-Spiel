def3c4c95bd942f18a55a8dd571b43f9091d250e
Raphael Tack <raphael.t7@gso.schule.koeln>
2023-12-20 15:54:17 +0100

    Folgende Änderungen wurden vorgenommen: Für die Wrapper-Tabelle wurde ein PK hinzugefügt.

--- Datenbank/dbo.Wrapper.sql
+++ Datenbank/dbo.Wrapper.sql
@@ -1,10 +1,10 @@
-﻿CREATE TABLE [dbo].Wrapper
-(
-	[User_ID] INT NOT NULL, 
-    [Statistic_ID] INT NOT NULL, 
-    [Date] DATE NOT NULL, 
-    [Time] INT NOT NULL, 
-    CONSTRAINT [Nutzer_FK] FOREIGN KEY (User_ID) REFERENCES Nutzer(User_ID), 
-    CONSTRAINT [Statistic_FK] FOREIGN KEY (Statistic_ID) REFERENCES Statistic(Statistic_ID),
+﻿CREATE TABLE [dbo].[Wrapper] (
+    [Wrapper_ID] INT NOT NULL PRIMARY KEY IDENTITY, 
+	[User_ID]      INT  NOT NULL,
+    [Statistic_ID] INT  NOT NULL,
+    [Date]         DATE NOT NULL,
+    [Time]         INT  NOT NULL,
+    CONSTRAINT [Nutzer_FK] FOREIGN KEY ([User_ID]) REFERENCES [dbo].[Nutzer] ([User_ID]),
+    CONSTRAINT [Statistic_FK] FOREIGN KEY ([Statistic_ID]) REFERENCES [dbo].[Statistic] ([Statistic_ID])
+);
 
-)
