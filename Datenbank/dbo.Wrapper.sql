7c4805ca6e0158a0bca48c268edc9ffdd027a9b1
Raphael Tack <raphael.t7@gso.schule.koeln>
2023-12-20 15:47:37 +0100

    Die "Wrapper" Tabelle wurde hinzugefügt. Das BD-Skript liegt nun vor: 	User_ID - FK 	Statistic_ID - FK 	Date - Date 	Time - Int

---
+++ Datenbank/dbo.Wrapper.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].Wrapper
+(
+	[User_ID] INT NOT NULL, 
+    [Statistic_ID] INT NOT NULL, 
+    [Date] DATE NOT NULL, 
+    [Time] INT NOT NULL, 
+    CONSTRAINT [Nutzer_FK] FOREIGN KEY (User_ID) REFERENCES Nutzer(User_ID), 
+    CONSTRAINT [Statistic_FK] FOREIGN KEY (Statistic_ID) REFERENCES Statistic(Statistic_ID),
+
+)
