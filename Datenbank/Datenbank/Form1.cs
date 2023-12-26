5469b017892b693ad85d6919184eb5ed4dc6145e
Raphael Tack <raphael.t7@gso.schule.koeln>
2023-12-26 18:05:11 +0100

    Im Folgenden wurden folgende Änderungen vorgenommen: 	- Eine Methode ShowUsers wurde definiert. 	- Eine Methode ShowStatistics wurde definiert.
    	Um Daten anzeigen zu lassen, müssen Datenobjekte erzeugt werden.
    	So wird mit Using ... gearbeitet.
    	So werden die Daten ausgegeben, was relevant für die Statistiken sind.
    	Die User - Daten müssen nicht ausgegeben werden.

--- Datenbank/Datenbank/Form1.cs
+++ Datenbank/Datenbank/Form1.cs
@@ -9,16 +9,20 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Configuration;
 using System.Data.SqlClient;
+using System.Data;
+using Datenbank.DBDateSetTableAdapters;
 
 namespace Datenbank
 {
     public partial class Form1 : Form
     {
+        SqlConnection sqlConnection;
+
         public Form1()
         {
-            SqlConnection sqlConnection;
-
             InitializeComponent();
+
+            // Nicht löschen !!! Ist der Verbindungsweg zur DB
             string connectionString = ConfigurationManager.ConnectionStrings["Datenbank.Properties.Settings.DBConnectionString"].ConnectionString;
 
             sqlConnection = new SqlConnection(connectionString);
@@ -27,6 +31,21 @@ namespace Datenbank
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        public void ShowUsers()
+        {
+            string querry = "SELECT * FROM USER1";
+            SqlDataAdapter sqlAdapter = new SqlDataAdapter(querry, sqlConnection);
+
+            // Um Daten auszugeben, müssen Objekte erzeugt werden. Bspw. DataTable --> ListBox
+            // Wie sollen die Daten ausgegeben werden...
+        }
+
+        public void ShowStatistics()
+        {
+            string querry = "SELECT * FROM STATISTIC";
+            SqlDataAdapter sqlAdapter = new SqlDataAdapter(querry, sqlConnection);
         }
     }
 }
