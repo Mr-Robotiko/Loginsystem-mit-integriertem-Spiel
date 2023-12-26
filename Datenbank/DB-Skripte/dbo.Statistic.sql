bdc14a2635a1379a86803ffe275c3ccbd0df6630
Raphael Tack <raphael.t7@gso.schule.koeln>
2023-12-26 16:14:21 +0100

    Die DB-Skripte wurden aktualisiert.

--- Datenbank/DB-Skripte/dbo.Statistic.sql
+++ Datenbank/DB-Skripte/dbo.Statistic.sql
@@ -3,6 +3,8 @@
     [Game_ID]      INT NOT NULL,
     [Total_time]   INT NOT NULL,
     [Highscore]    INT NOT NULL,
-    PRIMARY KEY CLUSTERED ([Statistic_ID] ASC)
+    [FK_User_ID] INT NOT NULL, 
+    PRIMARY KEY CLUSTERED ([Statistic_ID] ASC), 
+    CONSTRAINT [User_ID_FK] FOREIGN KEY (FK_User_ID) REFERENCES User1(User_ID)
 );
 
