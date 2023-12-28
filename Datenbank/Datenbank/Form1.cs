e6bd67f402a45282dc3c88684e890ad4b9250447
Raphael Tack <raphael.t7@gso.schule.koeln>
2023-12-28 17:11:39 +0100

    Die Datei ER-Modell im Eingang wurde geändert.

--- Datenbank/Datenbank/Form1.cs
+++ Datenbank/Datenbank/Form1.cs
@@ -64,5 +64,24 @@ namespace Datenbank
                 MessageBox.Show(e.ToString());
             }          
         }
+
+        public void AssociatedHighscore()
+        {
+            // Alle zusäzlichen SQL Anweisungen bitte im Try-Catch Format schreiben
+            // Sollten Daten abgefragt werden, die vielleicht verändert werden, kompiliert der Code trotzdem.
+            try
+            {
+                string querry = "SELECT u.Name, u.Prename, s.Highscore FROM User1 u INNER JOIN Statistic s ON u.User_ID = s.Statistic_ID";
+                SqlDataAdapter sqlAdapter = new SqlDataAdapter(querry, sqlConnection);
+
+                // Um Daten auszugeben, müssen Objekte erzeugt werden. Bspw. DataTable --> ListBox
+                // Wie sollen die Daten ausgegeben werden...
+            }
+
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+        }
     }
 }
