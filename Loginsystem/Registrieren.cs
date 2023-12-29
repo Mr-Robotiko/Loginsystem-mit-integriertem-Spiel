d0525292d335dc5d91e25679b8b3662e0a5e9960
Raphael Tack <raphael.t7@gso.schule.koeln>
2023-12-29 11:28:35 +0100

    Im Folgenden wurden folgende Änderungen vorgenommen: 	- Die Datenbankordner wurden gelöscht. 	- Die Idee einer lokalen DB wurde verworfen 	- Das Regristrationsform ist an die DB angebunden 		- Es können Nutzer angelegt werden mit Fehlertoleranz 	- Unter DB-Skripte ist ein komplettes DB-Skript vorhanden 	- Unter DB-Skripte ist eine .UDL Datei, um den ConnectionString zu finden 	- Die Nutzer wurden erweitert auf 11 Stück.

--- Loginsystem/Registrieren.cs
+++ Loginsystem/Registrieren.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.Sql;
 
 namespace Loginsystem
 {
@@ -15,13 +17,7 @@ namespace Loginsystem
         public Registrieren()
         {
             InitializeComponent();
-
-            // Placeholder Zuordnung
-            vorname_textBox.AddPlaceholder("Vorname");
-            name_textBox.AddPlaceholder("Name");
-            gebDatum_textBox.AddPlaceholder("Geburtsdatum");
-            benutzername_textBox.AddPlaceholder("Benutzername");
-            passwort_textBox.AddPlaceholder("Passwort");
+            AddPlaceholder();
         }
 
         private void beenden_button_Click(object sender, EventArgs e)
@@ -32,6 +28,71 @@ namespace Loginsystem
         private void zurueck_button_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        static string connectionString = @"Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=DB;Data Source=DESKTOP-G9GPCBT\SQLEXPRESS";
+
+        static SqlConnection connection = new SqlConnection(connectionString);
+
+        private void registrieren_button_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if(connection.State == ConnectionState.Closed)
+                {
+                    connection.Open();
+                }
+
+                if(vorname_textBox.Text != "" && name_textBox.Text != "" && gebDatum_textBox.Text != "" && passwort_textBox.Text != "" 
+                    && benutzername_textBox.Text != "")
+                {
+                    SqlCommand command = new SqlCommand("INSERT INTO User1 VALUES(@Prename, @Name, @Birthday,@Rights_Status, @Password)", connection);
+
+                    command.Parameters.AddWithValue("@Prename", vorname_textBox.Text);
+                    command.Parameters.AddWithValue("@Name", name_textBox.Text);
+                    command.Parameters.AddWithValue("@Birthday", Convert.ToDateTime(gebDatum_textBox.Text));
+                    command.Parameters.AddWithValue("@Password", passwort_textBox.Text);
+                    command.Parameters.AddWithValue("@Rights_Status", 'U');
+                   
+
+                    command.ExecuteNonQuery();
+                    connection.Close();
+
+                    MessageBox.Show("Erfolgreich regrisitriert");
+
+                    ClearFieldds();
+                    AddPlaceholder();
+                }
+
+                else
+                {
+                    MessageBox.Show("Du bist bereits regristriert");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+        private void ClearFieldds()
+        {
+            vorname_textBox.Text = "";
+            name_textBox.Text = "";
+            gebDatum_textBox.Text = "";
+            passwort_textBox.Text = "";
+            benutzername_textBox.Text = "";
+        }
+
+        private void AddPlaceholder()
+        {
+            // Placeholder Zuordnung
+            vorname_textBox.AddPlaceholder("Vorname");
+            name_textBox.AddPlaceholder("Name");
+            gebDatum_textBox.AddPlaceholder("Geburtsdatum");
+            benutzername_textBox.AddPlaceholder("Benutzername");
+            passwort_textBox.AddPlaceholder("Passwort");
         }
     }
 }
