5c0c2fea57f1d0540a014aaafec8284ca935aa71
Philip Danull <philip.d7@gso.schule.koeln>
2023-12-30 14:34:08 +0100

    Hintergrund mithilfe von einem Timer erstellt

--- Game_Spaceshooter/Game_Spaceshooter/Form1.Designer.cs
+++ Game_Spaceshooter/Game_Spaceshooter/Form1.Designer.cs
@@ -28,23 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.moveBgTimer = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
+            // 
+            // moveBgTimer
+            // 
+            this.moveBgTimer.Enabled = true;
+            this.moveBgTimer.Interval = 10;
+            this.moveBgTimer.Tick += new System.EventHandler(this.moveBgTimer_Tick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
             this.ClientSize = new System.Drawing.Size(778, 644);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximumSize = new System.Drawing.Size(800, 700);
             this.Name = "Form1";
             this.Text = "Spaceshooter";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Timer moveBgTimer;
     }
 }
 
--- Game_Spaceshooter/Game_Spaceshooter/Form1.cs
+++ Game_Spaceshooter/Game_Spaceshooter/Form1.cs
@@ -12,9 +12,67 @@ namespace Game_Spaceshooter
 {
     public partial class Form1 : Form
     {
+        PictureBox[] stars;
+        int backgroundspeed;
+        Random rnd;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //backgroundspeed ist die Geschwindigkeit der Sterne bzw.
+            //um wie viele Pixel sie sich nach unten bewegen
+            backgroundspeed = 4;
+            stars = new PictureBox[10];
+            rnd = new Random();
+
+            //Erstellt die Sterne und fügt sie dem Form hinzu
+            //Es gibt 10 Sterne, 5 kleine und 5 große, damit nicht alle gleich aussehen
+            for (int i = 0; i < stars.Length; i++)
+            {
+                stars[i] = new PictureBox();
+                stars[i].BorderStyle = BorderStyle.None;
+                stars[i].Location = new Point(rnd.Next(20, 500), rnd.Next(-10, 400));
+                if(i % 2 == 0)
+                {
+                    stars[i].Size = new Size(2, 2);
+                    stars[i].BackColor = Color.White;
+                }
+                else
+                {
+                    stars[i].Size = new Size(3, 3);
+                    stars[i].BackColor = Color.DarkGray;
+                }
+                this.Controls.Add(stars[i]);
+            }
+        }
+
+        //Timer, der die Sterne bewegt und dafür sorgt, dass sie wieder nach oben kommen
+        //Es gibt 2 verschiedene Geschwindigkeiten, damit es nicht so eintönig aussieht
+        private void moveBgTimer_Tick(object sender, EventArgs e)
+        {
+            for (int i = 0; i < stars.Length / 2; i++)
+            {
+                stars[i].Top += backgroundspeed;
+
+                if (stars[i].Top >= this.Height)
+                {
+                    stars[i].Top = -stars[i].Height;
+                }
+            }
+
+            for (int i = stars.Length / 2; i < stars.Length; i++)
+            {
+                stars[i].Top += backgroundspeed - 2;
+
+                if (stars[i].Top >= this.Height)
+                {
+                    stars[i].Top = -stars[i].Height;
+                }
+            }
         }
     }
 }
